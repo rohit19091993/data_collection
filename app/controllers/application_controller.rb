93cf9e1fea5ef61c3ccea30b0dfc6572e27a76a7
Rohit Kalavadiya <rohitkalavadiya9@gmail.com>
2018-08-30 05:01:12 -0700

    login and logout funcation development and pages create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+included Devise_Whitelist
 end
---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,11 @@
+module Devise_Whitelist 
+	extend ActiveSupport::Concern
+	included do
+		before_action :configure_permitted_parameters , if: :devise_controller?	
+	end
+	def configure_permitted_parameters
+	devise_parameter_sanitizer.permit(:sign_up , keys:[:first_name,:last_name,:mobile_no,:address,:state,:pin_code])
+	devise_parameter_sanitizer.permit(:account_update , keys:[:first_name,:last_name,:mobile_no,:address,:state,:pin_code])
+	end
+end
+
