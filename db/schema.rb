ffb5ae7fb15dd1c6ab467901f8da905d61bba564
Rohit Kalavadiya <rohitkalavadiya9@gmail.com>
2018-08-30 05:24:52 -0700

    Permission given

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_130652) do
+ActiveRecord::Schema.define(version: 2018_08_30_120855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2018_08_29_130652) do
     t.string "last_name"
     t.string "mobile_no"
     t.string "address"
-    #t.string "city"
+    t.string "city"
     t.string "state"
     t.string "pin_code"
     t.string "reset_password_token"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_08_29_130652) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
